ae31fc8e3be5c01879c6d6eec62d55b1d14ca6a5
motion <3600@163.com>
2022-06-10 11:34:41 +0800

    随便写了一下

---
+++ src/components/day1/01-test.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- 网页结构 -->
+  <div>你好,我是王小美</div>
+</template>
+
+<script>
+// 功能
+export default {};
+</script>
+<style lang="less" scoped>
+div {
+  color: aqua;
+  font-size: 40px;
+  text-align: center;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './components/day1/01-test.vue'
 
+// 引入样式文件
+import './styles/style.css'
 Vue.config.productionTip = false
 
 new Vue({
